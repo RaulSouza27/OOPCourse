b3c4f47e0d6908ab57356401e484e898a7d3983c
Thiago André Cardoso Silva <33556919+thiagoandrecardoso@users.noreply.github.com>
2024-01-04 17:11:55 -0300

    first commit

---
+++ poo/src/main/java/org/example/App.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.example.Model.Crianca;
+import org.example.Model.Esportista;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Esportista esportista = new Esportista("joao", "1234","Corredor");
+//        Crianca crianca = new Crianca("pedro", "1234", true);
+
+        Crianca crianca1 = new Crianca();
+    }
+}
