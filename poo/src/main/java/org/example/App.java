e2f5ff9c69ab0e39fec142604c93dbd1be3df529
Thiago André Cardoso Silva <33556919+thiagoandrecardoso@users.noreply.github.com>
2024-01-05 20:53:41 -0300

    Clean main

--- poo/src/main/java/org/example/App.java
+++ poo/src/main/java/org/example/App.java
@@ -1,7 +1,5 @@
 package org.example;
 
-import org.example.Model.Crianca;
-import org.example.Model.Esportista;
 
 /**
  * Hello world!
@@ -11,9 +9,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        Esportista esportista = new Esportista("joao", "1234","Corredor");
-//        Crianca crianca = new Crianca("pedro", "1234", true);
 
-        Crianca crianca1 = new Crianca();
     }
 }
